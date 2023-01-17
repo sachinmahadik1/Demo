0dddcdc0e4a533a771fc58b38669546708ae65d3
Sachin Mahadik <sachin.mahadik@kiya.ai>
2023-01-17 15:09:31 +0530

    initial commit

---
+++ DemowithMysql/About.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="DemowithMysql.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+
+    <asp:TextBox runat="server" ID="txtName" />  
+
+    <asp:Button Text="Save" runat="server" ID="btnsave" OnClick="btnsave_Click"/>
+    <asp:Label Text="" ID="lbl" runat="server" />
+
+    <asp:GridView runat="server" ID="grdTest" >   </asp:GridView>
+
+</asp:Content>
+
+
---
+++ DemowithMysql/About.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace DemowithMysql
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string MyConnection2 = "datasource=172.25.2.177;port=3306;username=rbc;password=mysql#123";
+            //Display query  
+            string Query = "select * from angularrbc.formmaster;";
+            MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+            MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+            //  MyConn2.Open();  
+            //For offline connection we weill use  MySqlDataAdapter class.  
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = MyCommand2;
+            DataTable dTable = new DataTable();
+            MyAdapter.Fill(dTable);
+            grdTest.DataSource = dTable;
+            grdTest.DataBind();
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+
+            
+
+            string MyConnection2 = "datasource=localhost;port=3306;username=root;password=mysql#123";
+            //Display query  
+            string Query = "select * from firstdb.employee;";
+            MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+            MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+            //  MyConn2.Open();  
+            //For offline connection we weill use  MySqlDataAdapter class.  
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = MyCommand2;
+            DataTable dTable = new DataTable();
+           MyAdapter.Fill(dTable);
+            grdTest.DataSource = dTable;
+            grdTest.DataBind();
+        }
+    }
+}
